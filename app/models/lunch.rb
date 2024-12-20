93d9432d37f44f6c7dd2e160dedd460c28f4be27
Julián Cipagauta <julian.cipa96@gmail.com>
2024-12-20 11:09:13 -0500

    Final commit

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -7,12 +7,12 @@ class Lunch < ApplicationRecord
   delegate :name, to: :participant, prefix: true, allow_nil: true
   delegate :name, to: :user, prefix: true, allow_nil: true
 
-  enum :kind, { kosher: "kosher", halal: "halal", vegetarian: "vegetarian", vegan: "vegan",
+  enum :kind, { kosher: "kosher", halal: "halal", vegetarian: "vegetarian", vegan: "vegan", peciterians: "peciterians",
                 without_restrictions: "without_restrictions" }
 
   class << self
     def humanized_kinds
-      kinds.map { |key, value| [ human_attribute_name("kind.#{value}"), key ] }
+      kinds.map { |key, value| [ human_attribute_name("kind/#{value}"), key ] }
     end
   end
 end
